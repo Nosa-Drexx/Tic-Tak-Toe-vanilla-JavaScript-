f1bdbd35d42d06ecc6ba924ef313a815c982c01f
Nosa-Drexx <96873980+Nosa-Drexx@users.noreply.github.com>
2022-04-17 18:58:40 +0100

    Update codes.js

--- codes.js
+++ codes.js
@@ -75,7 +75,7 @@ function AIplays() {
     shuffleArray(arr);
     pop = arr.pop();
     watchUserMovement();
-    //console.log(pop);
+    computerTriesToWin();
     return pop
 }
 
@@ -242,7 +242,6 @@ function watchUserMovement() {
             return pop = 1
         }
         else if (characterHolder[2].innerHTML == '') {
-
             //console.log(pop);
             return pop = 2;
         }
@@ -371,6 +370,168 @@ function watchUserMovement() {
     else if ((characterHolder[3].innerHTML == userCharacter && characterHolder[4].innerHTML == userCharacter) ||
         (characterHolder[3].innerHTML == userCharacter && characterHolder[5].innerHTML == userCharacter) ||
         (characterHolder[4].innerHTML == userCharacter && characterHolder[5].innerHTML == userCharacter) &&
+
+        (characterHolder[3].innerHTML == '' || characterHolder[4].innerHTML == '' || characterHolder[5].innerHTML == '')) {
+
+        if (characterHolder[3].innerHTML == '') {
+
+            return pop = 3;
+        }
+        else if (characterHolder[4].innerHTML == '') {
+            return pop = 4;
+        }
+        else if (characterHolder[5].innerHTML == '') {
+
+            return pop = 5;
+        }
+
+    }
+
+}
+
+function computerTriesToWin() {
+    if ((characterHolder[0].innerHTML == computerCharacter && characterHolder[1].innerHTML == computerCharacter) ||
+        (characterHolder[0].innerHTML == computerCharacter && characterHolder[2].innerHTML == computerCharacter) ||
+        (characterHolder[1].innerHTML == computerCharacter && characterHolder[2].innerHTML == computerCharacter) &&
+
+        (characterHolder[0].innerHTML == '' || characterHolder[1].innerHTML == '' || characterHolder[2].innerHTML == '')) {
+
+        if (characterHolder[0].innerHTML == '') {
+
+            return pop = 0;
+        }
+        else if (characterHolder[1].innerHTML == '') {
+            return pop = 1
+        }
+        else if (characterHolder[2].innerHTML == '') {
+
+            return pop = 2;
+        }
+    }
+
+    else if ((characterHolder[0].innerHTML == computerCharacter && characterHolder[3].innerHTML == computerCharacter) ||
+        (characterHolder[0].innerHTML == computerCharacter && characterHolder[6].innerHTML == computerCharacter) ||
+        (characterHolder[3].innerHTML == computerCharacter && characterHolder[6].innerHTML == computerCharacter) &&
+
+        (characterHolder[0].innerHTML == '' || characterHolder[3].innerHTML == '' || characterHolder[6].innerHTML == '')) {
+
+        if (characterHolder[0].innerHTML == '') {
+
+            return pop = 0;
+        }
+        else if (characterHolder[3].innerHTML == '') {
+            return pop = 3
+        }
+        else if (characterHolder[6].innerHTML == '') {
+
+            return pop = 6;
+        }
+
+    }
+
+    else if ((characterHolder[0].innerHTML == computerCharacter && characterHolder[4].innerHTML == computerCharacter) ||
+        (characterHolder[0].innerHTML == computerCharacter && characterHolder[8].innerHTML == computerCharacter) ||
+        (characterHolder[4].innerHTML == computerCharacter && characterHolder[8].innerHTML == computerCharacter) &&
+
+        (characterHolder[0].innerHTML == '' || characterHolder[4].innerHTML == '' || characterHolder[8].innerHTML == '')) {
+
+        if (characterHolder[0].innerHTML == '') {
+
+            return pop = 0;
+        }
+        else if (characterHolder[4].innerHTML == '') {
+            return pop = 4;
+        }
+        else if (characterHolder[8].innerHTML == '') {
+
+            return pop = 8;
+        }
+
+    }
+
+    else if ((characterHolder[2].innerHTML == computerCharacter && characterHolder[4].innerHTML == computerCharacter) ||
+        (characterHolder[2].innerHTML == computerCharacter && characterHolder[6].innerHTML == computerCharacter) ||
+        (characterHolder[4].innerHTML == computerCharacter && characterHolder[6].innerHTML == computerCharacter) &&
+
+        (characterHolder[2].innerHTML == '' || characterHolder[4].innerHTML == '' || characterHolder[6].innerHTML == '')) {
+
+        if (characterHolder[2].innerHTML == '') {
+
+            return pop = 2;
+        }
+        else if (characterHolder[4].innerHTML == '') {
+            return pop = 4;
+        }
+        else if (characterHolder[6].innerHTML == '') {
+
+            return pop = 6;
+        }
+
+    }
+
+    else if ((characterHolder[2].innerHTML == computerCharacter && characterHolder[5].innerHTML == computerCharacter) ||
+        (characterHolder[2].innerHTML == computerCharacter && characterHolder[8].innerHTML == computerCharacter) ||
+        (characterHolder[5].innerHTML == computerCharacter && characterHolder[8].innerHTML == computerCharacter) &&
+
+        (characterHolder[2].innerHTML == '' || characterHolder[5].innerHTML == '' || characterHolder[8].innerHTML == '')) {
+
+        if (characterHolder[2].innerHTML == '') {
+
+            return pop = 2;
+        }
+        else if (characterHolder[5].innerHTML == '') {
+            return pop = 5;
+        }
+        else if (characterHolder[8].innerHTML == '') {
+
+            return pop = 8;
+        }
+
+    }
+
+    else if ((characterHolder[6].innerHTML == computerCharacter && characterHolder[7].innerHTML == computerCharacter) ||
+        (characterHolder[6].innerHTML == computerCharacter && characterHolder[8].innerHTML == computerCharacter) ||
+        (characterHolder[7].innerHTML == computerCharacter && characterHolder[8].innerHTML == computerCharacter) &&
+
+        (characterHolder[6].innerHTML == '' || characterHolder[7].innerHTML == '' || characterHolder[8].innerHTML == '')) {
+
+        if (characterHolder[6].innerHTML == '') {
+
+            return pop = 6;
+        }
+        else if (characterHolder[7].innerHTML == '') {
+            return pop = 7;
+        }
+        else if (characterHolder[8].innerHTML == '') {
+
+            return pop = 8;
+        }
+
+    }
+
+    else if ((characterHolder[1].innerHTML == computerCharacter && characterHolder[4].innerHTML == computerCharacter) ||
+        (characterHolder[1].innerHTML == computerCharacter && characterHolder[7].innerHTML == computerCharacter) ||
+        (characterHolder[4].innerHTML == computerCharacter && characterHolder[7].innerHTML == computerCharacter) &&
+
+        (characterHolder[1].innerHTML == '' || characterHolder[4].innerHTML == '' || characterHolder[7].innerHTML == '')) {
+
+        if (characterHolder[1].innerHTML == '') {
+
+            return pop = 1;
+        }
+        else if (characterHolder[4].innerHTML == '') {
+            return pop = 4;
+        }
+        else if (characterHolder[7].innerHTML == '') {
+
+            return pop = 7;
+        }
+
+    }
+
+    else if ((characterHolder[3].innerHTML == computerCharacter && characterHolder[4].innerHTML == computerCharacter) ||
+        (characterHolder[3].innerHTML == computerCharacter && characterHolder[5].innerHTML == computerCharacter) ||
+        (characterHolder[4].innerHTML == computerCharacter && characterHolder[5].innerHTML == computerCharacter) &&
 
         (characterHolder[3].innerHTML == '' || characterHolder[4].innerHTML == '' || characterHolder[5].innerHTML == '')) {
 
